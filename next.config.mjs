d0b558afa3ccead177b56044ad52fadb23d7cd37
TKMhub <111867865+TKMhub@users.noreply.github.com>
2024-03-04 14:09:37 +0900

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
